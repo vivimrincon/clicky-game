7535c3c5b27895a5897e70933d6fe8b5f6db4567
ttalgihon <vivithefool@gmail.com>
2019-05-11 23:58:27 -0400

    creat css and props

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,5 @@ import React from "react";
 import '../styles/main.css';
 
 export default function Main(props) {
-  return (
-    <main className="container">{props.children}</main>
-  );
+  return <main className="container">{props.children}</main>;
 }
