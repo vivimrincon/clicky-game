538abed4bc100af2f380a749e92c81bcbf53595a
ttalgihon <vivithefool@gmail.com>
2019-05-11 21:51:34 -0400

    refactor app and index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react'; //React
-import './App.css'; //CSS
 import TopNav from './components/TopNav';
 
  export default function App() {
   return (
     <TopNav />
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p>
-    //       ttalgihon's react clicky-game
-    //     </p>
-    //     </header>
-    // </div>
   );
 }
