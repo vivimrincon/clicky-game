61bef1bc2679834e06ee86b312b2168b2cbe79e8
ttalgihon <vivithefool@gmail.com>
2019-05-14 01:03:14 -0400

    deploy

---
+++ build/precache-manifest.e248d870ef217837fdb17046909ff680.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8442cd256e1d255dad82a2f2d5efbefa",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b599a0bbf89e205f319f",
+    "url": "/clicky-game/static/css/main.a6ca8463.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/clicky-game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "b599a0bbf89e205f319f",
+    "url": "/clicky-game/static/js/main.f00e83f7.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
